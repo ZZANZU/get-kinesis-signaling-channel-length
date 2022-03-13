0ada1e32a15f8c8f6298334f75986bf9b23140d1
zzanzu <lees3301@gmail.com>
2022-03-13 22:16:12 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import boto3
+
+
+def get_channel_length():
+    client = boto3.client('kinesisvideo')
+    response = client.list_signaling_channels(
+        MaxResults=1000
+    )
+
+    total_channel_length = 0
+
+    try:
+        while response['NextToken']:
+            total_channel_length += len(response['ChannelInfoList'])
+
+            response = client.list_signaling_channels(
+                NextToken=response['NextToken']
+            )
+    except:
+        total_channel_length += len(response['ChannelInfoList'])
+
+    return total_channel_length
+
+
+if __name__ == '__main__':
+    print(get_channel_length())
